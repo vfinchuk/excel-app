ee46699aed78481ee70c72a469d536f9b275d2e8
vfinchuk <finchukvladimir@gmail.com>
2020-07-31 19:55:24 +0300

    Init class TableSelection

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,6 +2,7 @@ import ExcelComment from '@/core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
 import {shouldResize} from '@/components/table/table.functions'
+import TableSelection from '@/components/table/TableSelection'
 
 export default class Table extends ExcelComment {
   static className = 'excel__table'
@@ -17,6 +18,12 @@ export default class Table extends ExcelComment {
 
   toHTML() {
     return createTable()
+  }
+
+  init() {
+    super.init()
+
+    this.selection = new TableSelection()
   }
 
   onMousedown(event) {
