a80d23482314d29486bb8b70ddb921ee08b66e4c
vfinchuk <finchukvladimir@gmail.com>
2020-08-03 12:07:49 +0300

    Add Emmiter class

---
+++ src/core/Emitter.js
@@ -0,0 +1,60 @@
+export default class Emitter {
+  constructor() {
+    this.listeners = {}
+  }
+
+  /**
+   * (emit, fire, trigger)
+   * @param {string} event
+   * @param {args} args
+   * @return {boolean}dispatch
+   */
+  dispatch(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false
+    }
+
+    this.listeners[event].forEach(listener => {
+      listener(...args)
+    })
+    return true
+  }
+
+  /**
+   * on, listen
+   * @param {string} event
+   * @param {function} fn
+   * @return {function}
+   * formula.subscribe('table:select', () => {})
+   */
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || []
+    this.listeners[event].push(fn)
+    return () => {
+      this.listeners[event] = this.listeners[event]
+          .filter(listener => listener !== fn)
+    }
+  }
+}
+
+// const emitter = new Emmiter()
+// const unsubMyFunction = emitter.
+// subscribe('myFunction', data => console.log('MyFunction: ', data))
+//
+// emitter.dispatch('myFunction', 'myFunction data') // event not subscribe
+//
+// emitter
+// .dispatch('myFunction_NOT', 'myFunction data NOT') // event not subscribe
+//
+// setTimeout(() => {
+//   emitter.dispatch('myFunction', 'myFunction data after 2 sec')
+// }, 2000)
+//
+// setTimeout(() => {
+//   unsubMyFunction()
+// }, 3000)
+//
+// setTimeout(() => {
+//   emitter.dispatch('myFunction', 'myFunction data after 6 sec')
+// }, 6000)
+
